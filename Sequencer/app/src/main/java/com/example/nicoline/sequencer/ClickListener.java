e410ed426d3149be53ae1cb5f4f75eda694bd095
Elayoe <nicoline.larsen@webgolf.dk>
2016-04-24 09:11:39 +0200

    Renaming and methods

--- Sequencer/app/src/main/java/com/example/nicoline/sequencer/ClickListener.java
+++ Sequencer/app/src/main/java/com/example/nicoline/sequencer/ClickListener.java
@@ -1,8 +1,5 @@
 package com.example.nicoline.sequencer;
 
-import android.app.Activity;
-import android.media.SoundPool;
-import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -12,20 +9,20 @@ import android.widget.ImageView;
  */
 public class ClickListener implements View.OnClickListener {
 
-    MainActivity main;
+    private MainActivity main;
     //Handler h;
 
     //TODO: set these to x and y values
-    int barStartX;
-    int barStartY;
+    //Start x and y coords of sequencer status bar
+    private int barStartX;
+    private int barStartY;
 
     //Play settings
-    int priority = 1;
-    int no_loop = 0;
-    float normal_playback_rate = 1f;
+    private int soundPriority = 1;
+    private int no_loop = 0;
+    private float playback_rate = 1f; //1 is normal playback rate
 
-    ImageView view; //The image that has been clicked
-    //Start x and y coords of sequencer status bar
+    private ImageView view; //The image that has been clicked
 
     public ClickListener(ImageView v, MainActivity m) {
         view = v;
@@ -52,48 +49,11 @@ public class ClickListener implements View.OnClickListener {
         //TODO: make stop-button visible
 
         //TODO: play sounds:
-        //Play sequencer
-        Runnable r = new Runnable() {
-            @Override
-            public void run(){
-                for(int i = 0; i < main.sequencer.length; i++) {
-                    for(int j = 0; j < main.sequencer[0].length; j++) {
-                        if(main.sequencer[i][j].isToBePlayed == true) { //If we want to play this trackBlock
-                            main.soundPlayer.play(main.musicColumn[i],
-                                    main.sequencer[i][j].volume,
-                                    main.sequencer[i][j].volume,
-                                    priority,
-                                    no_loop,
-                                    normal_playback_rate);
-                        }
-                    }
-                    //TODO: Wait until next colum should be played
-                    //h.postDelayed(this, 1000);
-                }
-            }
-        };
-        r.run();
-        //h = new Handler();
-
-        /**
-         * Old sequencer code:
-        for(int i = 0; i < main.sequencer; i++) {
-            for(int j = 0; j < main.musicColumn.length; j++)
-            System.out.println("Iteration " + i+1 + ". Playing row number " + main.musicColumn[i] + " in current musicColumn");
-            if(main.sequencer)
-            main.soundPlayer.play(main.musicColumn[i],
-                    leftVolume,
-                    rightVolume,
-                    priority,
-                    no_loop,
-                    normal_playback_rate);
-        }*/
-
-        //MediaPlayer mp = MediaPlayer.create(this, R.raw.stone);
-        //mp.start();
+        playSoundsSimultaneously();
 
         //TODO: Start panning the sequencer status bar.
         //When finished two cycles (an orange and white background cycle), move back to start and repeat
+        startStatusBar();
 
         //---------------------------------------------------------------------
 
@@ -110,5 +70,58 @@ public class ClickListener implements View.OnClickListener {
         //TODO: stop sounds
 
         //TODO: Stop panning the sequencer status bar and reset it back to its original place
+    }
+
+    /**
+     * Method for playing the sequencer
+     */
+    void playSoundsSimultaneously(){
+        //Play sequencer
+        Runnable r = new Runnable() {
+            @Override
+            public void run(){
+                for(int i = 0; i < main.sequencer.length; i++) {
+                    for(int j = 0; j < main.sequencer[0].length; j++) {
+                        if(main.sequencer[i][j].isToBePlayed == true) { //If we want to play this trackBlock
+                            main.soundPlayer.play(main.musicColumn[i],
+                                    main.sequencer[i][j].volume,
+                                    main.sequencer[i][j].volume,
+                                    soundPriority,
+                                    no_loop,
+                                    playback_rate);
+                        }
+                    }
+                    //TODO: Wait until next colum should be played
+                    //h.postDelayed(this, 1000);
+                }
+            }
+        };
+        r.run();
+
+        //h = new Handler();
+
+        /**
+         * Old sequencer code:
+         for(int i = 0; i < main.sequencer; i++) {
+         for(int j = 0; j < main.musicColumn.length; j++)
+         System.out.println("Iteration " + i+1 + ". Playing row number " + main.musicColumn[i] + " in current musicColumn");
+         if(main.sequencer)
+         main.soundPlayer.play(main.musicColumn[i],
+         leftVolume,
+         rightVolume,
+         soundPriority,
+         no_loop,
+         playback_rate);
+         }*/
+
+        //MediaPlayer mp = MediaPlayer.create(this, R.raw.stone);
+        //mp.start();
+    }
+
+    /**
+     * Method that starts panning the status bar
+     */
+    void startStatusBar() {
+
     }
 }
